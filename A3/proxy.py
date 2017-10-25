e6596c6434673e7f59ac26cdb15c9eb68e533983
Chintav Shah <chintavmshah@gmail.com>
2017-10-25 14:11:14 -0600

    Parsing command line args

--- A3/proxy.py
+++ A3/proxy.py
@@ -1,13 +1,22 @@
 import socketserver
 import socket,threading
 import sys
-import urllib.request
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+	def handle(self):
+		logOption = sys.argv[]
 
 if __name__ == "__main__":
-   HOST, PORT = "localhost", int(sys.argv[1])
-   server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)
+   
+   HOST,SRC_PORT = "localhost", len(sys.argv) - 3
+  
+   DST_PORT = len(sys.argv) - 1 
+
+   server = socketserver.ThreadingTCPServer((HOST, SRC_PORT), MyTCPHandler)
    print("Proxy started on port %d... ", PORT)
    server.serve_forever()
+
 
 
 # for the HTTP response
@@ -16,4 +25,3 @@ if __name__ == "__main__":
 # self.request.sendall(bytearray(response.getheader("Date"), "UTF-8"))
 # self.request.sendall(bytearray(" from ", "UTF-8"))
 # response.getheader()  # I think this should work?
-
