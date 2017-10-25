78e1ac13f50406b1542666c3c48a9c71d8abce63
Chintav Shah <chintavmshah@gmail.com>
2017-10-25 13:35:46 -0600

    Started working on server

---
+++ A3/proxy.py
@@ -0,0 +1,9 @@
+import socketserver
+import socket,threading
+import sys
+
+if __name__ == "__main__":
+   HOST, PORT = "localhost", int(sys.argv[1])
+   server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)
+   print("Proxy started on port %d... ", PORT)
+   server.serve_forever()
