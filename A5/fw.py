5ecf8c2207d221ed0853ce8f9bf395f4fd3e009c
Raikita <rhassan@ucalgary.ca>
2017-11-16 13:36:47 -0700

    checking is rather broken haha

--- A5/fw.py
+++ A5/fw.py
@@ -1,5 +1,4 @@
 import sys, os
-import re
 
 # need to filter both incoming and outgoing packets
 
@@ -10,28 +9,46 @@ def firewall(rules, direction, ip, port, flag):
     d_match = 0
     ip_match = 0
     port_match = 0
-    est_only = False
+    establish = 0
+    DEBUG = False
+    #print(len(rules))
     for i in range(0, len(rules)):
+        #print("i",i+1)
+        #print("rules:", rules[i][2])
+
+        if DEBUG: print("direction:",rules[i][1], direction)
         if rules[i][1] == direction:
             d_match = 1
         else: d_match = 0
+
+        if DEBUG: print("ip:",rules[i][3], ip)
         if rules[i][3] == ip or rules[i][3] == "*":
             ip_match = 1
-        else: ip_match = 1
+        else: ip_match = 0
+
+        if DEBUG: print("port:", rules[i][4], port)
         if rules[i][4] == port or rules[i][4] == "*":
             port_match = 1
         else: port_match = 0
-        if rules[i][5] == 1:
-            est_only = True
-        else: est_only = False
+
+        #print("establish:", rules[i][5])
+        f = int(flag)
+        if rules[i][5] == "":
+            establish = 1
+        elif rules[i][5] == "established" and f == 1:
+            establish = 1
+        else:
+            establish = 0
 
         # figure out what to do with packet
         # if all match, do exactly as rule says
         # return which rule number to follow
-        if d_match == 1 and ip_match == 1 and port_match == 1 and (not est_only or (est_only and flag == 1)):
+        #print("Rules",rules[i])
+        if d_match == 1 and ip_match == 1 and port_match == 1 and establish == 1:
+            #print("Rules:",rules[i])
             return str(i+1) + " " + rules[i][2]
-
     # if reaches here, then no rules for this packet, so reject
+
     return "drop"
 
 
@@ -47,7 +64,9 @@ def firewall(rules, direction, ip, port, flag):
 # list of lists
 # line number for rule, direction, action, ip, port, flag
 
-
+#################
+##### Rules #####
+#################
 rules = [[]]
 
 linenum = 0
@@ -57,6 +76,7 @@ with open(config_file, "r") as f:
     line = sys.stdin.buffer.readline()
     for line in f:
         # split string to be put into a list
+        line = line.replace("\n", "")
         line.strip("\t")
         line = " ".join(line.split())
 
@@ -80,27 +100,35 @@ f.close()
 # packets read in from standard input
 # program reads from standard input until EOF, then should terminate
 
+###################
+##### Packets #####
+###################
+
 # direction, ip, port, flag
 packet = ["", "", "", ""]
-#with open(packet_file, "r") as f:
-line = sys.stdin.buffer.readline()
-for line in f:
-    direction = line.split(" ", 0)[1]
-    ip = line.split(" ", 1)[2]
-    port = line.split(" ", 2)[3]
-    flag = line.split(" ", 3)[4]
+for line in sys.stdin:
+    #print("packet:",line, end="", file=sys.stdout)
+    line = line.replace("\n", "")
+    line.strip("\t")
+
+    direction = line.split(" ", 1)[0]
+    ip = line.split(" ", 2)[1]
+    port = line.split(" ", 3)[2]
+    flag = line.split(" ", 4)[3]
+
 
     # time to accept, reject, or drop
     decision = firewall(rules, direction, ip, port, flag)
     if decision == "drop":
         # do the drop output
-        print("drop() "+direction+" "+ip+" "+port+" "+flag, file=stdout)
+        print("drop() "+direction+" "+ip+" "+port+" "+flag, file=sys.stdout)
     else:
         # if doesn't say drop, then do action
-        linenum = decision.split(" ", 0)[1]
-        action = decision.split(" ", 1)[2]
-        print(action+"("+linenum+") "+direction+" "+ip+" "+port+" "+flag, file=stdout)
-#f.close()
+        linenum = decision.split(" ", 1)[0]
+        action = decision.split(" ", 2)[1]
+        print(action+"("+linenum+") "+direction+" "+ip+" "+port+" "+flag, file=sys.stdout)
+
+
 
 
 
